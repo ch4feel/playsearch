b307890c9a8bd417e512cd21b1108b6d5f4b3fb7
LeeKeonJun <ch4feel@nhn.com>
2014-08-20 17:41:29 +0900

    날짜형식 수정, 모바일에서 2개씩 나오도록 스타일 수정

--- app.favor.js
+++ app.favor.js
@@ -5,7 +5,7 @@ var fs = require('fs'),
 request('http://www.naver.com', function(err, res, body){
 	var $html = cheerio.load(body);
 	var date = new Date();
-	var $ul = cheerio.load('<div class="area_rank"><h2 class="h_time">'+date.getFullYear()+'.'+(date.getMonth()+1)+'.'+date.getDate()+'. '+date.getHours()+':'+date.getMinutes()+'</h2><ol class="lst_rank">' + $html('#realrank').html() + '</ol></div>');
+	var $ul = cheerio.load('<div class="area_rank"><h2 class="h_time">'+date.getFullYear()+'.'+(date.getMonth()+1)+'.'+date.getDate()+'. '+date.getHours().zf(2)+':'+date.getMinutes().zf(2)+'</h2><ol class="lst_rank">' + $html('#realrank').html() + '</ol></div>');
 	var fshtml = fs.readFileSync('/home/pi/myhome/favor.html', 'utf8');
 
 
@@ -18,3 +18,12 @@ request('http://www.naver.com', function(err, res, body){
 	fs.writeFileSync('/home/pi/myhome/favor.html', $.html(), 'utf8');
 
 });
+
+String.prototype.string = function(len){
+	var s = '', i = 0;
+	while (i++ < len) { s += this; }
+	return s;
+};
+String.prototype.zf = function(len){return "0".string(len - this.length) + this;};
+Number.prototype.zf = function(len){return this.toString().zf(len);};
+
