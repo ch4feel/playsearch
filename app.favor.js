7e10977f7d6973161b4b93429d9dba59903a1601
LeeKeonJun <ch4feel@nhn.com>
2014-08-20 14:58:28 +0900

    실급검놀이 완성

--- app.favor.js
+++ app.favor.js
@@ -1,18 +1,22 @@
-var jsdom = require('jsdom'),
-    fs = require('fs');
+var fs = require('fs'),
+	request = require('request'),
+	cheerio = require('cheerio');
 
-jsdom.env("http://www.naver.com", ["http://code.jquery.com/jquery.js"], function(errs, window) {
-  var $ = window.$;
-  
-  var $ol = $('ol#realrank');
-  $ol.wrap('<ol class="lst_rank"></ol>');
-  $ol.find('#lastrank, .tx, .ic, .rk').remove();
+var html = '';
 
-  var html = fs.readFileSync('favor.html', 'utf8');
-  var $html = $(html);
+request('http://www.naver.com', function(err, res, body){
+	var $html = cheerio.load(body);
+	var date = new Date();
+	var $ul = cheerio.load('<div class="area_rank"><h2 class="h_time">'+date.getFullYear()+'년'+(date.getMonth()+1)+'월'+date.getDay()+'일 '+date.getHours()+'시'+date.getMinutes()+'분</h2><ol class="lst_rank">' + $html('#realrank').html() + '</ol></div>');
+	var fshtml = fs.readFileSync('favor.html', 'utf8');
 
-  console.log($ol.html());
-  console.log(html);
-  console.log($html.html());
+
+	$ul('.ic, .tx, .rk, #lastrank').remove();
+
+	var $ = cheerio.load(fshtml);
+
+	$('#wrap').prepend($ul.html());
+
+	fs.writeFileSync('favor.html', $.html(), 'utf8');
 
 });
--- app.server.js
+++ app.server.js
@@ -1,10 +1,12 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
 
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-
-  res.end('This is nodejs server!\n');
+	fs.readFile('favor.html', function(err, data){
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end(data);
+	})
 
 }).listen(8080, "127.0.0.1");
 
