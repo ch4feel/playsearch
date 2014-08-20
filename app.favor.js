dcac4b1dbfc1e99a747080f5fa421c1b69baaaf4
LeeKeonJun <ch4feel@nhn.com>
2014-08-20 15:28:40 +0900

    style 수정

--- app.favor.js
+++ app.favor.js
@@ -7,7 +7,7 @@ var html = '';
 request('http://www.naver.com', function(err, res, body){
 	var $html = cheerio.load(body);
 	var date = new Date();
-	var $ul = cheerio.load('<div class="area_rank"><h2 class="h_time">'+date.getFullYear()+'년'+(date.getMonth()+1)+'월'+date.getDay()+'일 '+date.getHours()+'시'+date.getMinutes()+'분</h2><ol class="lst_rank">' + $html('#realrank').html() + '</ol></div>');
+	var $ul = cheerio.load('<div class="area_rank"><h2 class="h_time">'+date.getFullYear()+'년'+(date.getMonth()+1)+'월'+date.getDate()+'일 '+date.getHours()+'시'+date.getMinutes()+'분</h2><ol class="lst_rank">' + $html('#realrank').html() + '</ol></div>');
 	var fshtml = fs.readFileSync('favor.html', 'utf8');
 
 
