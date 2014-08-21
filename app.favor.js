d151fe86e063438181bae4daa3fb6ff3363fcffa
LeeKeonJun <ch4feel@nhn.com>
2014-08-21 17:40:33 +0900

    초기유입수정

--- app.favor.js
+++ app.favor.js
@@ -25,9 +25,36 @@ var io = socketio.listen(server);
 
 console.log('Sockets are on');
 
-var mtimer = setInterval(getRank, 1000);
+io.sockets.on('connection',function(socket){
+	socket.on('first', function(){
+		request('http://www.naver.com', function(err, res, body){
+			var $html = cheerio.load(body);
+			var date = new Date();
+			var $ = cheerio.load($html('#realrank').html());
+			$('.ic, .tx, .rk, #lastrank').remove();
 
-function getRank(){
+			var json = {};
+			json.time = date.getFullYear()+'.'+(date.getMonth()+1)+'.'+date.getDate()+' '+date.getHours().zf(2)+':'+date.getMinutes().zf(2)+':'+date.getSeconds().zf(2);
+			json.type = 'first';
+			json.data = [];
+
+			$('li').each(function(){
+				json.data.push({
+					rank: $(this).attr('value'),
+					keyword: $(this).find('a').text(),
+					url: $(this).find('a').attr('href'),
+					type: $(this).attr('class')
+				});
+			})
+			socket.emit('first', json);
+		});
+	});
+	var mtimer = setInterval(function(){
+		getRank(socket);
+	}, 1000);
+})
+
+function getRank(socket){
 	request('http://www.naver.com', function(err, res, body){
 		var $html = cheerio.load(body);
 		var date = new Date();
@@ -36,8 +63,7 @@ function getRank(){
 
 		var json = {};
 		json.time = date.getFullYear()+'.'+(date.getMonth()+1)+'.'+date.getDate()+' '+date.getHours().zf(2)+':'+date.getMinutes().zf(2)+':'+date.getSeconds().zf(2);
-		//if(date.getMinutes() == 0 && date.getSeconds() == 0)
-		if(date.getSeconds()%10 == 0)
+		if(date.getMinutes() == 0 && date.getSeconds() == 0)
 			json.type = 'oclock';
 		else
 			json.type = 'normal';
@@ -52,45 +78,19 @@ function getRank(){
 			});
 		})
 
-		io.emit('realrank', json);
+		socket.emit('realrank', json);
 
 		if (json.type == 'oclock') {
-			var fsjson = eval(fs.readFileSync(filepath+'favor.json', 'utf8'));
+			if(date.getHours() == 6) {
+				var fsjson = [];
+			} else {
+				var fsjson = eval(fs.readFileSync(filepath+'favor.json', 'utf8'));
+			}
 			fsjson.push(json);
 			fs.writeFileSync(filepath+'favor.json', JSON.stringify(fsjson), 'utf8');
-		} else if(json.type == 'oclock' && date.getHours() == 6) {
-			fs.writeFileSync(filepath+'favor.json', '[]', 'utf8');
 		}
-
 	});
 }
-/*
-request('http://www.naver.com', function(err, res, body){
-	var $html = cheerio.load(body);
-	var date = new Date();
-	var $ = cheerio.load($html('#realrank').html());
-	$('.ic, .tx, .rk, #lastrank').remove();
-
-	var json = {};
-	json.time = date.getFullYear()+'.'+(date.getMonth()+1)+'.'+date.getDate()+' '+date.getHours().zf(2)+':'+date.getMinutes().zf(2);
-	json.data = [];
-
-	$('li').each(function(){
-		json.data.push({
-			rank: $(this).attr('value'),
-			keyword: $(this).find('a').text(),
-			url: $(this).find('a').attr('href'),
-			type: $(this).attr('class')
-		});
-	})
-
-	var fsjson = eval(fs.readFileSync(filepath+'favor.json', 'utf8'));
-
-	fsjson.push(json);
-
-	fs.writeFileSync(filepath+'favor.json', JSON.stringify(fsjson), 'utf8');
-
-});*/
 
 String.prototype.string = function(len){
 	var s = '', i = 0;
